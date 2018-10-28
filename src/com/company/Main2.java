39220257c5cd035ac7b3b49f87ef87e06db0f16d
wojciech.skublewski@wp.pl <wojciech.skublewski@wp.pl>
2018-10-28 19:08:19 +0100

    zadanie 1 i 2 (poprawione zadanie 2)

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -22,7 +22,7 @@ public class Main2 {
     static void lottoGame () {
 
         Scanner scanner = new Scanner(System.in);
-        int[] intArr = new int[6];
+        int[] intArr = {-1,-1,-1, -1, -1,-1};
         String supStr = "";
         int aNum = -1;
         int bNum = -1;
@@ -31,90 +31,84 @@ public class Main2 {
         int eNum = -1;
         int fNum = -1;
 
-        while (aNum == -1 || bNum == -1 || cNum == -1 || dNum == -1 || eNum == -1 || fNum == -1) {
+        while (intArr[0] == -1 || intArr[1]== -1 || intArr[2] == -1 || intArr[3] == -1 || intArr[4] == -1 || intArr[5] == -1) {
 
-            if (aNum == -1) {
+            if (intArr[0] == -1) {
                 System.out.println("Podaj 1 liczbe:");
                 supStr = scanner.nextLine();
                 if (supStr.equals(exit)) {
-                    aNum = bNum = cNum = dNum = eNum = fNum = 0;
+                    intArr[0] = intArr[1] = intArr[2] = intArr[3] = intArr[4] = intArr[5] = 0;
                     System.out.println("Program zakonczy sowje dzialanie");
                 } else if (!isIntegerNotmoreThan49(supStr)) {
                     System.out.println("To nie jest liczba z zakresu");
                 } else {
-                    aNum = Integer.valueOf(supStr);
-                    intArr[0] = aNum;
+                    intArr[0] = Integer.valueOf(supStr);
                 }
-            } else if (bNum == -1) {
+            } else if (intArr[1] == -1) {
                 System.out.println("Podaj 2 liczbe:");
                 supStr = scanner.nextLine();
                 if (supStr.equals(exit)) {
-                    aNum = bNum = cNum = dNum = eNum = fNum = 0;
+                    intArr[0] = intArr[1] = intArr[2] = intArr[3] = intArr[4] = intArr[5] = 0;
                     System.out.println("Program zakonczy sowje dzialanie");
                 } else if (!isIntegerNotmoreThan49(supStr)) {
                     System.out.println("To nie jest liczba z zakresu");
-                } else if (aNum == Integer.valueOf(supStr)) {
+                } else if (intArr[0] == Integer.valueOf(supStr)) {
                     System.out.println("Podana liczba jest juz wprowadzone");
                 } else {
-                    bNum = Integer.valueOf(supStr);
-                    intArr[1] = bNum;
+                    intArr[1] = Integer.valueOf(supStr);
                 }
 
-            } else if (cNum == -1) {
+            } else if (intArr[2] == -1) {
                 System.out.println("Podaj 3 liczbe:");
                 supStr = scanner.nextLine();
                 if (supStr.equals(exit)) {
-                    aNum = bNum = cNum = dNum = eNum = fNum = 0;
+                    intArr[0] = intArr[1] = intArr[2] = intArr[3] = intArr[4] = intArr[5] = 0;
                     System.out.println("Program zakonczy sowje dzialanie");
                 } else if (!isIntegerNotmoreThan49(supStr)) {
                     System.out.println("To nie jest liczba z zakresu");
-                } else if (aNum == Integer.valueOf(supStr) || bNum == Integer.valueOf(supStr)) {
+                } else if (intArr[0] == Integer.valueOf(supStr) || intArr[1] == Integer.valueOf(supStr)) {
                     System.out.println("Podana liczba jest juz wprowadzone");
                 } else {
-                    cNum = Integer.valueOf(supStr);
-                    intArr[2] = cNum;
+                    intArr[2] = Integer.valueOf(supStr);
                 }
-            } else if (dNum == -1) {
+            } else if (intArr[3] == -1) {
                 System.out.println("Podaj 4 liczbe:");
                 supStr = scanner.nextLine();
                 if (supStr.equals(exit)) {
-                    aNum = bNum = cNum = dNum = eNum = fNum = 0;
+                    intArr[0] = intArr[1] = intArr[2] = intArr[3] = intArr[4] = intArr[5] = 0;
                     System.out.println("Program zakonczy sowje dzialanie");
                 } else if (!isIntegerNotmoreThan49(supStr)) {
                     System.out.println("To nie jest liczba z zakresu");
-                } else if (aNum == Integer.valueOf(supStr) || bNum == Integer.valueOf(supStr) || cNum == Integer.valueOf(supStr)) {
+                } else if (intArr[0] == Integer.valueOf(supStr) || intArr[1] == Integer.valueOf(supStr) || intArr[2] == Integer.valueOf(supStr)) {
                     System.out.println("Podana liczba jest juz wprowadzone");
                 } else {
-                    dNum = Integer.valueOf(supStr);
-                    intArr[3] = dNum;
+                    intArr[3] = Integer.valueOf(supStr);
                 }
-            } else if (eNum == -1) {
+            } else if (intArr[4] == -1) {
                 System.out.println("Podaj 5 liczbe:");
                 supStr = scanner.nextLine();
                 if (supStr.equals(exit)) {
-                    aNum = bNum = cNum = dNum = eNum = fNum = 0;
+                    intArr[0] = intArr[1] = intArr[2] = intArr[3] = intArr[4] = intArr[5] = 0;
                     System.out.println("Program zakonczy sowje dzialanie");
                 } else if (!isIntegerNotmoreThan49(supStr)) {
                     System.out.println("To nie jest liczba z zakresu");
-                } else if (aNum == Integer.valueOf(supStr) || bNum == Integer.valueOf(supStr) || cNum == Integer.valueOf(supStr) || dNum == Integer.valueOf(supStr)) {
+                } else if (intArr[0] == Integer.valueOf(supStr) || intArr[1] == Integer.valueOf(supStr) || intArr[2] == Integer.valueOf(supStr) || intArr[3] == Integer.valueOf(supStr)) {
                     System.out.println("Podana liczba jest juz wprowadzone");
                 } else {
-                    eNum = Integer.valueOf(supStr);
-                    intArr[4] = eNum;
+                    intArr[4] = Integer.valueOf(supStr);
                 }
-            } else if (fNum == -1) {
+            } else if (intArr[5] == -1) {
                 System.out.println("Podaj 6 liczbe:");
                 supStr = scanner.nextLine();
                 if (supStr.equals(exit)) {
-                    aNum = bNum = cNum = dNum = eNum = fNum = 0;
+                    intArr[0] = intArr[1] = intArr[2] = intArr[3] = intArr[4] = intArr[5] = 0;
                     System.out.println("Program zakonczy sowje dzialanie");
                 } else if (!isIntegerNotmoreThan49(supStr)) {
                     System.out.println("To nie jest liczba z zakresu");
-                } else if (aNum == Integer.valueOf(supStr) || bNum == Integer.valueOf(supStr)|| cNum == Integer.valueOf(supStr) || dNum == Integer.valueOf(supStr)|| eNum == Integer.valueOf(supStr))  {
+                } else if (intArr[0] == Integer.valueOf(supStr) || intArr[1] == Integer.valueOf(supStr)|| intArr[2] == Integer.valueOf(supStr) || intArr[3] == Integer.valueOf(supStr)|| intArr[4] == Integer.valueOf(supStr))  {
                     System.out.println("Podana liczba jest juz wprowadzone");
                 } else {
-                    fNum = Integer.valueOf(supStr);
-                    intArr[5] = fNum;
+                    intArr[5] = Integer.valueOf(supStr);
                 }
 
             }
@@ -126,12 +120,31 @@ public class Main2 {
             System.out.println(Arrays.toString(intArr));
 
             Random r = new Random();
-            aNum = r.nextInt(50);
-            bNum = r.nextInt(50);
-            cNum = r.nextInt(50);
-            dNum = r.nextInt(50);
-            eNum = r.nextInt(50);
-            fNum = r.nextInt(50);
+
+            while (aNum == -1 || bNum == -1 || cNum == -1 || dNum == -1 || eNum == -1 || fNum == -1) {
+                aNum = r.nextInt(50);
+                bNum = r.nextInt(50);
+                if (aNum==bNum) {
+                    bNum=-1;
+                }
+                cNum = r.nextInt(50);
+                if (aNum==cNum || bNum==cNum) {
+                    cNum=-1;
+                }
+                dNum = r.nextInt(50);
+                if (aNum==dNum || bNum==dNum || cNum==dNum) {
+                    dNum=-1;
+                }
+                eNum = r.nextInt(50);
+                if (aNum==eNum || bNum==eNum || cNum==eNum || dNum==eNum) {
+                    eNum=-1;
+                }
+                fNum = r.nextInt(50);
+                if (aNum==fNum || bNum==fNum || cNum==fNum || dNum==fNum || eNum==fNum) {
+                    fNum=-1;
+                }
+            }
+
             int licznik = 0;
 
             System.out.println(aNum + " " + bNum + " " + cNum + " " + dNum + " " + eNum + " " + fNum + " ");
