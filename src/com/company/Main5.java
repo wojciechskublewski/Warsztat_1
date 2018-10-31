f06c679d7185c6bd426851100532227596c5166d
wojciech.skublewski@wp.pl <wojciech.skublewski@wp.pl>
2018-10-31 21:39:51 +0100

    Zadanie 3

---
+++ src/com/company/Main5.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+public class Main5 {
+
+
+    public static void main(String[] args) {
+
+       Connection conect = Jsoup.connect("http://www.onet.pl");
+
+    }
+}
